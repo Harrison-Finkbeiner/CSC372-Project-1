443d0cb2d719b92d83e4fbd1732ddfcdb2624477
Harrison-Finkbeiner <hfinkbeiner@email.arizona.edu>
2023-10-04 21:07:21 -0700

    Added mongo db to the be used as the database for the application.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'recipe/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'recipe#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
